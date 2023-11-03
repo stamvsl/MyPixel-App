0547f7f279dcb2603253d06e822ff29a2edff1e4
stamvsl <stam.vsl@gmail.com>
2023-11-03 12:06:44 +0200

    uri

--- server/connection.js
+++ server/connection.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
-main().then(()=>console.log ("database has connected successfully") ).catch(err => console.log(err));
+import "dotenv/config";
+main()
+  .then(() => console.log("database has connected successfully"))
+  .catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(`mongodb+srv://stamvsl:mongopass@cluster0.qq6blos.mongodb.net/?retryWrites=true&w=majority`);
+  await mongoose.connect(process.env.MONGODB_URI);
 }
-export default main()
+export default main();
