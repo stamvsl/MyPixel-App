50b9f89ea310dc0f14f18d99dc6dff527624a565
stamvsl <stam.vsl@gmail.com>
2023-11-07 11:33:25 +0200

    added .env

--- client/src/components/feed.js
+++ client/src/components/feed.js
@@ -1,82 +1,83 @@
-import React, { useState, useEffect, useParams } from 'react';
-import axios from 'axios';
-import {Link} from "react-router-dom"
-import Form from "./form"
-import Header from './header';
-import Footer from './footer';
-import "./feed.css"
+import React, { useState, useEffect, useParams } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Form from "./form";
+import Header from "./header";
+import Footer from "./footer";
+import "./feed.css";
 
 const Feed = () => {
+  console.log(process.env.NODE_ENV);
+  // const {id} = useParams
+  const path =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3001"
+      : process.env.REACT_APP_BACKEND_URL;
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`${path}/allPosts`)
+      .then((res) => res.data)
+      .then((posts) => {
+        console.log("posts: ", posts);
+        setData(posts);
+      })
+      .catch((e) => console.log("error", e));
+  }, []);
 
-// const {id} = useParams
-    
-const [data, setData] = useState(null)
-useEffect(() => {
-    axios.get("http://localhost:3001/allPosts")
-        .then((res)=> res.data)
-        .then((posts)=>{
-            console.log("posts: ", posts)
-            setData(posts)
-        })
-        .catch((e)=> console.log("error",e))
-  },[]);
-
-
-  const removePost = (id) =>{
-    axios.delete(`http://localhost:3001/posts/delete/${id}`)
-    .then((res)=>{
+  const removePost = (id) => {
+    console.log("path ", path);
+    axios
+      .delete(`${path}/posts/delete/${id}`)
+      .then((res) => {
         console.log("Success", res);
-    
-    }).catch((err)=>{
-        console.log("err", err)
-    })
-    window.location.replace('http://localhost:3000');
-  }
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+    const window_path =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.REACT_APP_BASE_PATH;
+    window.location.replace(window_path);
+  };
 
-   
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="feed">
+          <div className="feedTop">
+            <img src="../images/usam.png" />
+            <h2>WE WANT YOUR ART</h2>
+            <Link to="/form">
+              <button>Post here</button>
+            </Link>
+          </div>
 
-// const removePost= async(id) =>{
-//     const response =await axios.delete(`http://localhost:3001/posts/delete/${id}`)
-//     if(response.data.data){
-//         setData(response.data.data)
-//     }
-// } 
-
-
-
-    return(
-        <div>
-            <Header />
-            <div className='container'>
-                <div className='feed'>
-                    <div className='feedTop'>
-                        
-                        <img src='../images/usam.png' />
-                        <h2>WE WANT YOUR ART</h2>
-                        <Link to = "/form">
-                                <button>Post here</button>
-                    </Link>
-                       
-                    </div>
-                   
-                    <br /><br /><br />
-                        <div>
-                            {data && data.map((val)=> 
-                            <div key={val._id} className = "post">
-                                <div> {val?.text} </div>
-                                <div> <img src={val?.image }/> </div>
-                                <button onClick={()=>removePost(val._id)}> Remove this post</button>
-                            </div>) }
-                        </div>
+          <br />
+          <br />
+          <br />
+          <div>
+            {data &&
+              data.map((val) => (
+                <div key={val._id} className="post">
+                  <div> {val?.text} </div>
+                  <div>
+                    {" "}
+                    <img src={val?.image} />{" "}
+                  </div>
+                  <button onClick={() => removePost(val._id)}>
+                    {" "}
+                    Remove this post
+                  </button>
                 </div>
-            </div>
-            <Footer />
-            
-            
+              ))}
+          </div>
         </div>
-    )
-
-    
-
-}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 export default Feed;
--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,60 +1,64 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-import {  Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 const Form = () => {
-    const [image, setImage] = useState("");
-    const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+  const [text, setText] = useState("");
 
-    const onInputChange = (e) =>{
-        console.log("e.target: ", e.target.files)
-        if(e.target?.files?.length > 0){
-
-            setImage(e.target.files[0])
-        }
-        
-        setText(e.target.value)
+  const onInputChange = (e) => {
+    console.log("e.target: ", e.target.files);
+    if (e.target?.files?.length > 0) {
+      setImage(e.target.files[0]);
     }
 
+    setText(e.target.value);
+  };
 
-    const onFormSubmit = (e) =>{
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("image", image);
-        formData.append("text", text);
-        const config = {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
-        }
-        axios.post("http://localhost:3001/posts/new", formData, config)
-            .then((res)=>{
-                console.log("Success", res);
-            
-            }).catch((err)=>{
-                console.log("err", err)
-            })
-            
-        window.location.replace('http://localhost:3000');
-    }
-    
-        return(
-            <div className='formWrapper'> 
-            <div className='form'>
-                
-                <form onSubmit={onFormSubmit}>
-                    
-                    <h1>Post your art here (Suggested 480x480px)</h1>
-                    <input type="file" name="image" onChange={onInputChange}/>
-                    <input type="text" name="text" onChange={onInputChange}/>
-                    
-                    <button type='submit'>upload</button>
-                     
-                </form>
-            </div>
-            </div>
-        );
-}
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image);
+    formData.append("text", text);
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    const path =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3001"
+        : process.env.REACT_APP_BACKEND_URL;
+    axios
+      .post(`${path}/posts/new`, formData, config)
+      .then((res) => {
+        console.log("Success", res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
 
-export default Form
+    const window_path =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.REACT_APP_BASE_PATH;
+    window.location.replace(window_path);
+  };
+
+  return (
+    <div className="formWrapper">
+      <div className="form">
+        <form onSubmit={onFormSubmit}>
+          <h1>Post your art here (Suggested 480x480px)</h1>
+          <input type="file" name="image" onChange={onInputChange} />
+          <input type="text" name="text" onChange={onInputChange} />
+
+          <button type="submit">upload</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
